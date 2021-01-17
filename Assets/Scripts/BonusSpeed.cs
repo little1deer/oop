4ed9122ed0108345882fb6634585a139ff9a882a
little1deer <deer.nmsk@gmail.com>
2021-01-17 23:55:00 +0300

    add unity project

---
+++ Assets/Scripts/BonusSpeed.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+public class BonusSpeed : MonoBehaviour
+{
+    #region Fields
+    
+    
+    
+    #endregion
+
+
+    #region UnityMethods
+
+    private void Start()
+    {
+		
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    #endregion
+
+
+    #region Methods
+    
+    
+    
+    #endregion
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    #region Fields
+    
+    
+    
+    #endregion
+
+
+    #region UnityMethods
+
+    private void Start()
+    {
+		
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    #endregion
+
+
+    #region Methods
+    
+    
+    
+    #endregion
+}
